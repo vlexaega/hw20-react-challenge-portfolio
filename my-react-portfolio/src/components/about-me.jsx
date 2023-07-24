dd59a25ac3cac103b7f1f4d3a6c7808dd18c702f
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-24 17:48:45 -0400

    adds the about-me component and display on main page

---
+++ my-react-portfolio/src/components/about-me.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const AboutMe = () =>{
+    return (
+        <div className="about-me">
+            <h2>About Me</h2>
+            <p>Hello World and welcome to my page! I'm a banker turned web developer originally from The Bahamas - but I call 
+            New York City home now. I enjoy travelling and spending time at the beach when I can. I'm growing
+            my skills in web development and I'm excited to show you my work! Links to some of my recent projects
+            can be found below. 
+            Feel free to contact me by clicking any of the contact links below so we can get in touch! Hope you enjoy!</p>
+        </div>
+    )
+}
+
+export default AboutMe;
